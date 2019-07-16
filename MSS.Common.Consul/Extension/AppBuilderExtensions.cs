76deb9481076e4cd161d21ffa32a862458804f0a
hyx <huyanxinag@outlook.com>
2019-07-16 11:13:03 +0800

    Init Consul

---
+++ MSS.Common.Consul/Extension/AppBuilderExtensions.cs
@@ -0,0 +1,55 @@
+﻿using Consul;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace MSS.Common.Consul
+{
+    public static class AppBuilderExtensions
+    {
+        public static IApplicationBuilder RegisterConsul(this IApplicationBuilder app, IApplicationLifetime lifetime,IOptions<ConsulServiceEntity>  serviceEntity)
+        {
+            var consulClient = new ConsulClient(x => x.Address = new Uri($"http://{serviceEntity.Value.ConsulIP}:{serviceEntity.Value.ConsulPort}"));
+            var httpCheck = new AgentServiceCheck()
+            {
+                DeregisterCriticalServiceAfter = TimeSpan.FromSeconds(5),
+                Interval = TimeSpan.FromSeconds(10),
+                HTTP = $"http://{serviceEntity.Value.IP}:{serviceEntity.Value.Port}/health",
+                Timeout = TimeSpan.FromSeconds(5),
+                
+             
+            };
+
+            // Register service with consul
+            var registration = new AgentServiceRegistration()
+            {
+                Checks = new[] { httpCheck },
+                ID = Guid.NewGuid().ToString(),
+                Name = serviceEntity.Value.ServiceName,
+                Address = serviceEntity.Value.IP,
+                Port = serviceEntity.Value.Port,
+                
+                Tags = new[] { $"urlprefix-/{serviceEntity.Value.ServiceName}" }
+            };
+
+            consulClient.Agent.ServiceRegister(registration).Wait();
+            lifetime.ApplicationStopping.Register(() =>
+            {
+                consulClient.Agent.ServiceDeregister(registration.ID).Wait();
+            });
+
+            app.Map("/health", HealthMap);
+            return app;
+        }
+
+        private static void HealthMap(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("OK");
+            });
+        }
+    }
+}
---
+++ MSS.Common.Consul/Extension/ConsulServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+
+namespace MSS.Common.Consul
+{
+    public static class ConsulServiceCollectionExtensions
+    {
+        public static IServiceCollection AddConsulService(this IServiceCollection services, IConfiguration _configuration)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+
+            services.AddOptions();
+            services.Configure<ConsulServiceEntity>(_configuration.GetSection(nameof(ConsulServiceEntity)));
+            services.AddTransient<IServiceDiscoveryProvider, ConsulServiceProvider>();
+            return services;
+        }
+    }
+}
